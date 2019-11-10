8e7081cb969aa0efb2c2450aa6e207670bba2178
Stuart Ianna <stuian@protonmail.com>
2019-11-10 20:32:39 +0200

    Another cleanup

---
+++ template/stm32f767zi/src/main.cpp
@@ -0,0 +1,29 @@
+#include <stm32f76xxx_core.h>
+#include <arm_math.h>
+
+#define LED_PORT    GPIOB
+#define LED_PIN     7 
+
+void delayMs(uint16_t ms);
+
+int main(void){
+
+    GPIO_pinSetup(GPIO_DO,LED_PORT,LED_PIN);
+
+    while(1){
+
+        delayMs(300);
+        GPIO_pinToggle(LED_PORT,LED_PIN);
+
+    }
+}
+
+// Millisecond delay is an estimate
+void delayMs(uint16_t ms){
+
+    for(uint16_t i = 0; i < ms; i++){
+        for(uint16_t j = 0; j < 0x1FFF; j++){
+            __ASM("NOP");
+        }
+    }
+}
