f5901f309ebff1b7626c8c6b1fea1e99ef9e9d2f
Stuart Ianna <stuian@protonmail.com>
2019-11-24 17:04:18 +0200

    Added FIFO documentation

--- lib/common/inc/fifo.hpp
+++ lib/common/inc/fifo.hpp
@@ -3,39 +3,101 @@
 
 #include <cstdint>
 
+/**
+  @addtogroup Common
+  @{
+*/
+
+/*!
+    @addtogroup FIFO
+    @brief Circular FIFO buffer template class
+		@details There are two modes of operation for implementation.
+			1. **Automatic output** - The first entry of the fifo is sent to the output
+			function as soon as the first entry is written with put. Successive
+			entries can be retreived by using the get function.
+			2. **Triggered output** - Contents are continually added the the buffer.
+			The size of the buffer can be monitored and all contents sent to the output
+			function using the flush function.
+
+			An item is not added to the buffer if it is already full.
+
+    @author Stuart Ianna
+    @date Novamber 2019
+    @warning FIFO memory buffer size must be a exponention power of 2 (2,4,8,16 ..)
+    @par ISR Rising edge example
+    @include example_gpio_isr.cpp
+  @{
+*/
+
 template <typename T>
 class cFIFO{
 
 public:
 
+/*!
+    @brief Fifo modes of operation.
+*/
 	typedef enum{
-		FIFO_MODE_AUTO,
-		FIFO_MODE_TRIGGER,
-		FIFO_MODE_DUMP,
+		FIFO_MODE_AUTO,							//!< Fifo contents are dumped automatically.
+		FIFO_MODE_TRIGGER,					//!< Fifo contents are only dumped with #get or #flush
 	}Fifo_Mode;
 
+/*!
+    @brief Fifo return types
+*/
 	typedef enum{
-		FIFO_RETURN_ERROR,
-		FIFO_RETURN_OK,
+		FIFO_RETURN_ERROR,					//!< An error occured
+		FIFO_RETURN_OK,							//!< No error occured
 	}Fifo_Return;
 
+/*!
+    @brief Contructor, set up a new FIFO object.
+    @param buffer Pointer to buffer memory location. Must be an exponential
+		power of 2. (2,4,8,16,32 ..)
+    @param mode The mode of operation.
+		@param size Must be the same size as the allocated memory location.
+		@param output This function is called when #get of #flush is used.
+    @return N/A
+*/
 	cFIFO(T *buffer, Fifo_Mode mode, uint16_t size, void (*output)(T));
-	uint16_t put(T);
+/*!
+    @brief Add an item to the memory buffer
+		@param in The item to add. Note the item will not be added if the buffer
+		is already full.
+    @return The size of the buffer after the item was added.
+*/
+	uint16_t put(T in);
+/*!
+    @brief Send the next item to the output function.
+    @return The size of the buffer after the item was retreived.
+*/
 	uint16_t get();
+/*!
+    @brief Get the current size used in the buffer
+    @return The size of the buffer.
+*/
 	uint16_t size();
+/*!
+    @brief Send all the buffer contents to the output function.
+		@details Uses a while loop to push until all objects are sent.
+*/
 	void flush();
+/*!
+    @brief Reset the buffer.
+		@details This effectivly sets the head and tail locations to the
+		same address. It doesn't clear each memory location (set to zero.)
+*/
 	void reset();
 
 private:
-	T *buffer;
-	uint16_t head;
-	uint16_t tail;
-	uint16_t mask;
-	uint16_t mode;
-	bool idle;
-	void (*output)(T);
+	T *buffer;									//!< A pointer to the start of the buffer memory location.
+	uint16_t head;							//!< Relative location of the start of the buffer.
+	uint16_t tail;							//!< Relative location of the end of the buffer.
+	uint16_t mask;							//!< Mask to 'wrap around' the buffer
+	uint16_t mode;							//!< The current mode of the object.
+	bool idle;									//!< FIFO object status
+	void (*output)(T);					//!< The output function
 };
-
 
 template<typename T>
 cFIFO<T>::cFIFO(T *buffer, Fifo_Mode mode, uint16_t size, void (*output)(T)){
@@ -104,5 +166,8 @@ void cFIFO<T>::flush(){
 		empty = this->idle;
 	}
 }
+
+/**@}*/
+/**@}*/
 
 #endif
