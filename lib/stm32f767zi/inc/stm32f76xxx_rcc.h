032b9907e69ac89a8640d9674911e57b135023cd
Stuart Ianna <stuian@protonmail.com>
2019-12-04 22:09:36 +0200

    Added pll, hsi, hse and bus clock speed configuration

--- lib/stm32f767zi/inc/stm32f76xxx_rcc.h
+++ lib/stm32f767zi/inc/stm32f76xxx_rcc.h
@@ -8,9 +8,18 @@
   @{
 */
 
-/*! 
+/*!
     @addtogroup RCC
-    @brief Module for reset and clock control (RCC) configuration. Applicable for ARM based MCU.
+    @brief Module for reset and clock control (RCC) configuration.
+		@details This module can be used for setting enabling / disabling
+		peripheral clocks as well as setting system clock and peripheral
+		bus speeds.
+
+		By far the best way to determing what parameters are required when
+		setting up the PLL or bus clocks is to use the STM32CubeMX graphical
+		tool. The prescalers and settings there can be used directly in the
+		clock configuration structure.
+
     @author Stuart Ianna
     @date November 2019
     @warning none
@@ -18,7 +27,7 @@
   @{
 */
 
-/*! 
+/*!
     @brief RCC peripheral clocks.
     @details This abstraction allows for common naming and a single function to be
     used to enable and disable clocks.
@@ -42,18 +51,154 @@ typedef enum{
     RCC_USART3      = RCC_APB1ENR_USART3EN,         //!<USART 3 clock
 }RCC_Peripheral;
 
-/*! 
+/*!
+    @brief Used to select the system clock source in RCC_setSystemClock
+*/
+typedef enum{
+	RCC_SYSCLOCK_HSI = 0x00,				//!< High speed internal oscillator (HSI)
+	RCC_SYSCLOCK_HSE = 0x01,				//!< High speed external oscillator (HSE)
+	RCC_SYSCLOCK_PLL = 0x10,				//!< Phase locked loop (PLL)
+}Rcc_Sys_Clock;
+
+/*!
+    @brief Used to select the input to the phase locked loop (PLL)
+*/
+typedef enum{
+	RCC_PLL_INPUT_HSI = 0x00,				//!< HSI is used as PLL input.
+	RCC_PLL_INPUT_HSE = 0x01,				//!< HSE is used as PLL input.
+}Rcc_Pll_Input;
+
+/*!
+    @brief Peripheral busses which can have different clock rates.
+		@details This can be used with RCC_getClockSpeed() to quesry
+		the speed of the given bus.
+*/
+typedef enum{
+	RCC_APB1_PERIPHERAL,						//!< APB1 peripheral clock bus (PCKL1)
+	RCC_APB1_TIMER,									//!< APB1 timer clock bus
+	RCC_APB2_PERIPHERAL,						//!< APB2 peripheral clock bus (PCKL1)
+	RCC_APB2_TIMER,									//!< APB2 timer clock bus
+	RCC_SYSTEM_CLOCK,								//!< System clock
+	RCC_CORTEX_CLOCK,								//!< Cortex system clock (systick)
+}Rcc_Clock;
+
+/*!
+    @brief APB prescalers for the peripheral busses
+		@details These prescalers are for APB1 and APB2
+*/
+typedef enum{
+	RCC_PRESCALER_APB_0 	= 0x3,					//!< No prescaler on the bus
+	RCC_PRESCALER_APB_2 	= 0x4,					//!< Divisor of 2
+	RCC_PRESCALER_APB_4 	= 0x5,					//!< Divisor of 4
+	RCC_PRESCALER_APB_8 	= 0x6,					//!< Divisor of 8
+	RCC_PRESCALER_APB_16 	= 0x7,					//!< Divisor of 16
+}Rcc_Bus_Apb_Prescaler ;
+
+/*!
+    @brief AHB prescalers for the system bus.
+		@details This prescaler effects all clocks except the ethernet bus.
+*/
+typedef enum{
+	RCC_PRESCALER_AHB_0 		= 0x0,					//!< No prescaler on the bus
+	RCC_PRESCALER_AHB_2 		= 0x8,					//!< Divisor of 2
+	RCC_PRESCALER_AHB_4 		= 0x9,					//!< Divisor of 4
+	RCC_PRESCALER_AHB_8 		= 0xA,					//!< Divisor of 8
+	RCC_PRESCALER_AHB_16 		= 0xB,					//!< Divisor of 16
+	RCC_PRESCALER_AHB_64 		= 0xC,					//!< Divisor of 64
+	RCC_PRESCALER_AHB_128 	= 0xD,					//!< Divisor of 128
+	RCC_PRESCALER_AHB_256 	= 0xE,					//!< Divisor of 256
+	RCC_PRESCALER_AHB_512 	= 0xF,					//!< Divisor of 512
+}Rcc_Bus_Ahb_Prescaler ;
+
+/*!
+    @brief Pll output divisors.
+		@details This divisor should be used in conjuction with other
+		pll scalers to ensure the system bus doesn't exceed 216Mhz.'
+*/
+typedef enum{
+	RCC_PLL_DIVISOR_2 = 0x00,								//!< Divisor of 2 on PLL output
+	RCC_PLL_DIVISOR_4 = 0x01,								//!< Divisor of 4 on PLL output
+	RCC_PLL_DIVISOR_6 = 0x02,								//!< Divisor of 6 on PLL output
+	RCC_PLL_DIVISOR_8 = 0x03,								//!< Divisor of 8 on PLL output
+}Rcc_Pll_Divisor;
+
+/*!
+    @brief Structure containing required parameeters for RCC_setSystemClock.
+		@details These parameters should be chosed to conform to the limits setout
+		in the MCU datasheet. Alternatively, STM32CubeMX can be used. Multiple
+		templates exist in this file for 'standard' configurations.
+*/
+typedef struct{
+	Rcc_Sys_Clock systemClock; 					//!< The input source for the main clock
+	Rcc_Pll_Input pllInput; 						//!< The input source for the PLL
+	Rcc_Bus_Ahb_Prescaler ahbPrescale;	//!< The main prescaler for the AHB bus
+	uint8_t pllM;												//!< VCO denominator
+	uint16_t pllN;  										//!< VCO numerator
+	Rcc_Pll_Divisor pllP;						     //!< General clock divisor
+	uint8_t pllQ;												//!< USB OTG FS, RNG clock divisor
+	uint8_t pllR;												//!< DSI clock output
+	uint32_t target;										//!< The target freqeuncy, used to set flash wait states.
+}Rcc_ClockConfig;
+
+extern const Rcc_ClockConfig template_HSI_16;		//<! Predefined template for HSI clock @ 16Mhz
+extern const Rcc_ClockConfig template_HSI_8; 		//<! Predefined template for HSI clock @ 16Mhz
+extern const Rcc_ClockConfig template_HSI_1; 		//<! Predefined template for HSI clock @ 16Mhz
+extern const Rcc_ClockConfig template_PLL_48; 	//<! Predefined template for PLL clock @ 48Mhz
+extern const Rcc_ClockConfig template_PLL_108;	//<! Predefined template for PLL clock @ 108Mhz
+extern const Rcc_ClockConfig template_PLL_216;	//<! Predefined template for PLL clock @ 216Mhz
+
+/*!
+    @brief Change the speed and source of the system clock.
+		@details These parameters passed should be chosed to conform to the limits setout
+		in the MCU datasheet. Alternatively, STM32CubeMX can be used. Multiple
+		templates exist in this file for 'standard' configurations.
+		in the MCU datasheet. Alternatively, STM32CubeMX can be used.
+		@param config Clock configuration parameters defined by Rcc_ClockConfig
+		@note This function doesn't disable any other clock sources which may
+		not be used.
+		@return Returns SUCCESS if new clock speed matches the target member
+		of Rcc_ClockConfig. ERROR if a mismatch occurs.
+*/
+uint8_t RCC_setSystemClock(Rcc_ClockConfig *config);
+/*!
+    @brief Get the speed of a defined clock bus in Hz.
+		@param clock Target alternate clock bus.
+    @return The target clock speed in Hz.
+*/
+uint32_t RCC_getClockSpeed(Rcc_Clock clock);
+/*!
+    @brief Set the clock bus prescaler.
+		@details Prescalers can only be set for the AHB, APB1 and APB2 peripheral busses.
+		An attempt to set the prescaler on another bus will have no effect.
+		@param clock Target alternate clock bus.
+		@param prescaler The prescale value to set.
+    @return The resulting frequency of the peripheral bus.
+*/
+uint32_t RCC_setBusPrescaler(Rcc_Clock clock, Rcc_Bus_Apb_Prescaler prescaler);
+/*!
+    @brief Set the clock speed of a bus to its maximum allowed value.
+		@details This function is only relevent for the AHB1 and AHB2 peripheral
+		busses. The AHB1 bus has a maximum speed of 54Mhz. The AHB2 bus has a maximum
+		speed of 108Mhz.
+		This function is used internally by RCC_setSystemClock to ensure the
+		peripheral busses are not set at too high a frequency.
+		@param clock Target alternate clock bus to set (AHB1 or AHB2).
+    @return The resulting frequency of the peripheral bus.
+*/
+uint32_t RCC_setBusMaximum(Rcc_Clock clock);
+/*!
     @brief Enables a given peripheral clock.
     @param peripheral The clock to enable, defined by #RCC_Peripheral
     @return SUCCESS if enable, FAIL if not.
 */
-bool clockEnable(RCC_Peripheral peripheral);
-/*! 
+bool RCC_clockEnable(RCC_Peripheral peripheral);
+/*!
     @brief Disables a given peripheral clock.
     @param peripheral The clock to disable, defined by #RCC_Peripheral
     @return SUCCESS if enable, FAIL if not.
 */
-bool clockDisable(RCC_Peripheral peripheral);
+bool RCC_clockDisable(RCC_Peripheral peripheral);
+
 
 /**@}*/
 /**@}*/
--- lib/stm32f767zi/src/stm32f76xxx_rcc.c
+++ lib/stm32f767zi/src/stm32f76xxx_rcc.c
@@ -1,6 +1,309 @@
 #include "stm32f76xxx_rcc.h"
 
-bool clockEnable(RCC_Peripheral peripheral){
+void rcc_enableHSE(void);
+void rcc_disableHSE(void);
+void rcc_enableHSI(void);
+void rcc_disableHSI(void);
+void rcc_configurePLL(Rcc_ClockConfig *config);
+void rcc_setAhbPrescaler(Rcc_Bus_Ahb_Prescaler p);
+
+const Rcc_ClockConfig template_HSI_16 = {
+	.systemClock 	= RCC_SYSCLOCK_HSI,
+	.pllInput 		= RCC_PLL_INPUT_HSI,
+	.ahbPrescale  = RCC_PRESCALER_AHB_0,
+	.pllM 				= 0,
+	.pllN 				= 0,
+	.pllP 				= RCC_PLL_DIVISOR_2,
+	.pllQ 				= 0,
+	.pllR 				= 0,
+	.target  			= 16000000
+};
+
+const Rcc_ClockConfig template_HSI_8 = {
+	.systemClock 	= RCC_SYSCLOCK_HSI,
+	.pllInput 		= RCC_PLL_INPUT_HSI,
+	.ahbPrescale  = RCC_PRESCALER_AHB_2,
+	.pllM 				= 0,
+	.pllN 				= 0,
+	.pllP 				= RCC_PLL_DIVISOR_2,
+	.pllQ 				= 0,
+	.pllR 				= 0,
+	.target  			= 8000000
+};
+
+const Rcc_ClockConfig template_HSI_1 = {
+	.systemClock 	= RCC_SYSCLOCK_HSI,
+	.pllInput 		= RCC_PLL_INPUT_HSI,
+	.ahbPrescale  = RCC_PRESCALER_AHB_16,
+	.pllM 				= 0,
+	.pllN 				= 0,
+	.pllP 				= RCC_PLL_DIVISOR_2,
+	.pllQ 				= 0,
+	.pllR 				= 0,
+	.target  			= 1000000
+};
+
+const Rcc_ClockConfig template_PLL_48 = {
+	.systemClock 	= RCC_SYSCLOCK_PLL,
+	.pllInput 		= RCC_PLL_INPUT_HSI,
+	.ahbPrescale  = RCC_PRESCALER_AHB_0,
+	.pllM 				= 8,
+	.pllN 				= 192,
+	.pllP 				= RCC_PLL_DIVISOR_8,
+	.pllQ 				= 8,
+	.pllR 				= 2,
+	.target  			= 48000000
+};
+
+const Rcc_ClockConfig template_PLL_108 = {
+	.systemClock 	= RCC_SYSCLOCK_PLL,
+	.pllInput 		= RCC_PLL_INPUT_HSI,
+	.ahbPrescale  = RCC_PRESCALER_AHB_0,
+	.pllM 				= 8,
+	.pllN 				= 216,
+	.pllP 				= RCC_PLL_DIVISOR_4,
+	.pllQ 				= 9,
+	.pllR 				= 2,
+	.target  			= 108000000
+};
+
+const Rcc_ClockConfig template_PLL_216 = {
+	.systemClock 	= RCC_SYSCLOCK_PLL,
+	.pllInput 		= RCC_PLL_INPUT_HSI,
+	.ahbPrescale  = RCC_PRESCALER_AHB_0,
+	.pllM 				= 8,
+	.pllN 				= 216,
+	.pllP 				= RCC_PLL_DIVISOR_2,
+	.pllQ 				= 9,
+	.pllR 				= 2,
+	.target  			= 216000000
+};
+
+void rcc_enableHSI(void){
+
+	RCC->CR |= RCC_CR_HSION;
+	while(!(RCC->CR & RCC_CR_HSIRDY)){};
+}
+
+void rcc_disableHSI(void){
+
+	RCC->CR &= ~RCC_CR_HSION;
+}
+
+void rcc_enableHSE(void){
+
+	RCC->CR |= RCC_CR_HSION;
+	while(!(RCC->CR & RCC_CR_HSIRDY)){};
+}
+
+void rcc_disableHSE(void){
+
+	RCC->CR &= ~RCC_CR_HSEON;
+}
+
+void rcc_setAhbPrescaler(Rcc_Bus_Ahb_Prescaler p){
+	MODIFY_REG(RCC->CFGR,RCC_CFGR_HPRE_Msk,p << RCC_CFGR_HPRE_Pos);
+}
+
+void rcc_configurePLL(Rcc_ClockConfig *config){
+
+	// Disable PLL so it can be configured
+	RCC->CR &= ~RCC_CR_PLLON;
+
+	// PLL Configuration
+	if(config->pllInput == RCC_PLL_INPUT_HSI){
+		rcc_enableHSI();
+		RCC->PLLCFGR &= ~RCC_PLLCFGR_PLLSRC;
+	}else{
+		rcc_enableHSE();
+		RCC->PLLCFGR |= RCC_PLLCFGR_PLLSRC;
+	}
+
+	// Set divisors
+	MODIFY_REG(RCC->PLLCFGR,
+			(RCC_PLLCFGR_PLLR_Msk |
+			 RCC_PLLCFGR_PLLQ_Msk |
+			 RCC_PLLCFGR_PLLP_Msk |
+			 RCC_PLLCFGR_PLLN_Msk |
+			 RCC_PLLCFGR_PLLM_Msk ),(
+				 config->pllR << RCC_PLLCFGR_PLLR_Pos |
+				 config->pllQ << RCC_PLLCFGR_PLLQ_Pos |
+				 config->pllP << RCC_PLLCFGR_PLLP_Pos |
+				 config->pllN << RCC_PLLCFGR_PLLN_Pos |
+				 config->pllM << RCC_PLLCFGR_PLLM_Pos ));
+
+
+	// Enable and wait for PLL to be ready
+	RCC->CR |= RCC_CR_PLLON;
+	while(!(RCC->CR & RCC_CR_PLLRDY));
+
+	// Adjust wait statues
+	FLASH_setWaitStates(config->target);
+
+	// Set PLL as source and wait to be ready
+	MODIFY_REG(RCC->CFGR,RCC_CFGR_SW_Msk,RCC_CFGR_SW_1);
+	while(!(RCC->CFGR & RCC_CFGR_SWS_1)){};
+
+}
+
+uint8_t RCC_setSystemClock(Rcc_ClockConfig *config){
+
+	// Enable the required clock source
+	switch(config->systemClock){
+		default:
+		case RCC_SYSCLOCK_HSI:
+
+			// Is HSI already the clock source?
+			if(!(RCC->CFGR & RCC_CFGR_SW_Msk)){
+
+				rcc_setAhbPrescaler(config->ahbPrescale);
+				SystemCoreClockUpdate();
+				return !(SystemCoreClock == config->target);
+			}
+
+			rcc_enableHSI();
+			RCC->CFGR = RCC->CFGR & ~RCC_CFGR_SW_Msk;
+			while(RCC->CFGR & RCC_CFGR_SWS_Msk){};
+
+			rcc_setAhbPrescaler(config->ahbPrescale);
+			FLASH_setWaitStates(config->target);
+			SystemCoreClockUpdate();
+
+			// Update the peripheral bus prescalers to none.
+			RCC_setBusPrescaler(RCC_APB1_PERIPHERAL,RCC_PRESCALER_APB_0);
+			RCC_setBusPrescaler(RCC_APB2_PERIPHERAL,RCC_PRESCALER_APB_0);
+			return !(SystemCoreClock == config->target);
+
+		case RCC_SYSCLOCK_HSE:
+
+			// IS HSE already the clock source?
+			if(!(RCC->CFGR & RCC_CFGR_SWS_0)){
+
+				rcc_setAhbPrescaler(config->ahbPrescale);
+				SystemCoreClockUpdate();
+				return !(SystemCoreClock == config->target);
+			}
+			rcc_enableHSE();
+			RCC->CFGR = RCC->CFGR & ~RCC_CFGR_SW_Msk;
+			RCC->CFGR |= RCC_CFGR_SW_0;
+			while(RCC->CFGR & RCC_CFGR_SWS_0){};
+
+			rcc_setAhbPrescaler(config->ahbPrescale);
+			FLASH_setWaitStates(config->target);
+			SystemCoreClockUpdate();
+
+			// Update the peripheral bus prescalers to none.
+			RCC_setBusPrescaler(RCC_APB1_PERIPHERAL,RCC_PRESCALER_APB_0);
+			RCC_setBusPrescaler(RCC_APB2_PERIPHERAL,RCC_PRESCALER_APB_0);
+			return !(SystemCoreClock == config->target);
+
+		case RCC_SYSCLOCK_PLL:
+
+			// Need to switch back to HSI to change PLL settings
+			if((RCC->CFGR & RCC_CFGR_SWS_Msk) == RCC_CFGR_SWS_1){
+				rcc_enableHSI();
+				RCC->CFGR = RCC->CFGR & ~RCC_CFGR_SW_Msk;
+				while(RCC->CFGR & RCC_CFGR_SWS_Msk){};
+			}
+			break;
+	}
+
+	rcc_configurePLL(config);
+
+	rcc_setAhbPrescaler(config->ahbPrescale);
+	SystemCoreClockUpdate();
+
+	RCC_setBusMaximum(RCC_APB1_PERIPHERAL);
+	RCC_setBusMaximum(RCC_APB2_PERIPHERAL);
+
+	return !(SystemCoreClock == config->target);
+}
+
+uint32_t RCC_setBusMaximum(Rcc_Clock clock){
+
+	uint8_t prescaleTarget = 0;
+
+	switch(clock){
+		case RCC_APB1_PERIPHERAL:
+			// APB1 bus has a maximum speed of 54MHz
+			prescaleTarget = SystemCoreClock / 54000000;
+			break;
+		case RCC_APB2_PERIPHERAL:
+			// APB2 bus has a maximum speed of 108MHz
+			prescaleTarget = SystemCoreClock / 108000000;
+			break;
+		default:
+			return 0;
+	}
+
+	if(prescaleTarget > 2){
+		return RCC_setBusPrescaler(clock,RCC_PRESCALER_APB_4);
+	}else if(prescaleTarget > 0){
+		return RCC_setBusPrescaler(clock,RCC_PRESCALER_APB_2);
+	}else{
+		return RCC_setBusPrescaler(clock,RCC_PRESCALER_APB_0);
+	}
+
+	return 0;
+}
+
+uint32_t RCC_getClockSpeed(Rcc_Clock clock){
+
+	switch(clock){
+		case RCC_APB1_PERIPHERAL:
+
+			if(!(RCC->CFGR & RCC_CFGR_PPRE1_Msk)){
+				return SystemCoreClock;
+			}
+			return SystemCoreClock >> ((((RCC->CFGR & RCC_CFGR_PPRE1_Msk) >> RCC_CFGR_PPRE1_Pos)) - 3);
+
+		case RCC_APB1_TIMER:
+
+			if((((RCC->CFGR & RCC_CFGR_PPRE1_Msk) >> RCC_CFGR_PPRE1_Pos)) - 3){
+				return 2*SystemCoreClock >> ((((RCC->CFGR & RCC_CFGR_PPRE1_Msk) >> RCC_CFGR_PPRE1_Pos)) - 3);
+			}
+			return SystemCoreClock >> ((((RCC->CFGR & RCC_CFGR_PPRE1_Msk) >> RCC_CFGR_PPRE1_Pos)) - 3);
+
+		case RCC_APB2_PERIPHERAL:
+
+			if(!(RCC->CFGR & RCC_CFGR_PPRE2_Msk)){
+				return SystemCoreClock;
+			}
+			return SystemCoreClock >> ((((RCC->CFGR & RCC_CFGR_PPRE2_Msk) >> RCC_CFGR_PPRE2_Pos)) - 3);
+
+		case RCC_APB2_TIMER:
+
+			if((((RCC->CFGR & RCC_CFGR_PPRE2_Msk) >> RCC_CFGR_PPRE2_Pos)) - 3){
+				return 2*SystemCoreClock >> ((((RCC->CFGR & RCC_CFGR_PPRE2_Msk) >> RCC_CFGR_PPRE2_Pos)) - 3);
+			}
+			return SystemCoreClock >> ((((RCC->CFGR & RCC_CFGR_PPRE2_Msk) >> RCC_CFGR_PPRE2_Pos)) - 3);
+
+		case RCC_SYSTEM_CLOCK:
+			return SystemCoreClock;
+		case RCC_CORTEX_CLOCK:
+			return SystemCoreClock;
+	}
+	return 0;
+}
+
+uint32_t RCC_setBusPrescaler(Rcc_Clock clock, Rcc_Bus_Apb_Prescaler prescale){
+
+	switch(clock){
+		case RCC_APB1_PERIPHERAL:
+			MODIFY_REG(RCC->CFGR,RCC_CFGR_PPRE1_Msk,prescale << RCC_CFGR_PPRE1_Pos);
+			return SystemCoreClock >> ((((RCC->CFGR & RCC_CFGR_PPRE1_Msk) >> RCC_CFGR_PPRE1_Pos)) - 3);
+
+		case RCC_APB2_PERIPHERAL:
+			MODIFY_REG(RCC->CFGR,RCC_CFGR_PPRE2_Msk,prescale << RCC_CFGR_PPRE2_Pos);
+			return SystemCoreClock >> ((((RCC->CFGR & RCC_CFGR_PPRE2_Msk) >> RCC_CFGR_PPRE2_Pos)) - 3);
+
+		default:
+			break;
+	}
+	return 0;
+}
+
+bool RCC_clockEnable(RCC_Peripheral peripheral){
 
     switch(peripheral){
         case RCC_AHB1ENR_GPIOAEN:
@@ -39,7 +342,7 @@ bool clockEnable(RCC_Peripheral peripheral){
     return SUCCESS;
 }
 
-bool clockDisable(RCC_Peripheral peripheral){
+bool RCC_clockDisable(RCC_Peripheral peripheral){
 
     switch(peripheral){
         case RCC_AHB1ENR_GPIOAEN:
